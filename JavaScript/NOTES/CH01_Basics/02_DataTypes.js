d6ba44ca609793a084125279ef4bbb39dc0d3924
TusharChaudhary-tcc <tusharchaudhary6160@gmail.com>
2026-07-19 22:46:39 +0530

    Add JavaScript data types notes

---
+++ JavaScript/NOTES/CH01_Basics/02_DataTypes.js
@@ -0,0 +1,40 @@
+// To treat all JS code as new version but nowadays it automtically treat all code as new version
+"use strict";
+
+
+// alert(3+3) // we are using node.js, not browser and this works on browser
+
+
+/* There are 2 types of Datatypes in JS :-
+  # Primitive:
+    1. Number- Stores integers and decimal numbers
+    2. String- Stores text (inside " ", ' ', or ` `)
+    3. Boolean- Stores only two values: true or false
+    4. Undefined- A variable is declared but not assigned a value.
+    5. Null- Represents an intentional empty value.
+    6. BigInt- Stores very large integers.
+    7. Symbol- Stores unique and immutable values
+
+  # Non-Primitive:
+    Object (Array, Date, Function, etc.) 
+*/
+
+
+
+// typeof Operator - Used to check the data type of a value.
+console.log(typeof 10);             // number
+console.log(typeof "Tushar");       // string
+console.log(typeof null);              // object
+console.log(typeof undefined);         // undefined
+
+
+
+console.table([typeof 18, typeof "Tushar", typeof null, typeof undefined]);
+
+
+
+
+/* For more study use these docs
+-> Mdn docs-  https://developer.mozilla.org/en-US/docs/Web/JavaScript
+-> ECMAscript-  https://tc39.es/ecma262/
+*/
