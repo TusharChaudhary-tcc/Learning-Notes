27b5d65190fd482e02c5e000f33a0878d6d1df78
TusharChaudhary-tcc <tusharchaudhary6160@gmail.com>
2026-07-20 22:55:27 +0530

    Add JavaScript operators notes

---
+++ JavaScript/NOTES/CH01_Basics/04_Operations.js
@@ -0,0 +1,37 @@
+let a=22;
+let b=7;
+
+// console.log(a+b);          
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);     // (a to the power of b) like acts as a^b
+
+
+// To add (concatenate) two or more strings.
+
+let str1="Tushar";
+let str2="Chaudhary";
+
+let str3= str1 +" "+ str2;
+// console.log(str3);
+
+console.log("\n");
+
+
+console.log("1" + 2);              // Output:-12
+console.log(1 + "2");              // Output:-12
+console.log("1" + "2");            // Output:-12
+console.log("1" + 2 + 2);          // Output:- 122     // If string present in first so the after all values treated as string.
+console.log(1 + 2 + "4");          // Output:- 34      // If string present in last so the before all values treated as number.
+
+console.log("\n");
+
+
+
+// Prefix and Suffix
+let counter=100;
+console.log(counter++);    // Post increment : Firstly print the value and then increase that
+console.log(++counter);    // Pre increment : Firstly increase the value and then print that
+
